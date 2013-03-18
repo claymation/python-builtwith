a0f85c8b97be7fac04389370f212198a806f59c7
Clay McClure <clay@daemons.net>
2013-03-18 15:30:45 -0400

    Specify builtwith module in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name = "python-builtwith",
-    version = "0.1.0",
+    version = "0.1.1",
     description = "BuiltWith API version 1 client",
     author = "Clay McClure",
     author_email = "clay@daemons.net",
@@ -15,7 +15,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Topic :: Internet :: WWW/HTTP',
     ],
-    packages = find_packages(),
+    py_modules = ['builtwith'],
     install_requires = [
         'requests==1.1.0',
     ],
