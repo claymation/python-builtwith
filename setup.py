45a0f7463752856a6d766317d614f4a257f800b9
Clay McClure <clay@daemons.net>
2020-04-11 23:33:35 -0700

    Un-pin requirements

--- setup.py
+++ setup.py
@@ -2,26 +2,26 @@ from setuptools import setup
 
 setup(
     name = "python-builtwith",
-    version = "0.2.2",
+    version = "1.0.0",
     description = "BuiltWith API versions 1, 2 and 7 client",
     author = "Clay McClure, Jon Gaulding, Andrew Harris",
     author_email = "clay@daemons.net",
     url = "https://github.com/claymation/python-builtwith",
     classifiers = [
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 7 - Inactive',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Internet :: WWW/HTTP',
     ],
     py_modules = ['builtwith'],
     install_requires = [
-        'requests==2.20.0',
+        'requests',
     ],
     test_suite = 'nose.collector',
     tests_require = [
-        'httpretty==0.5.12',
-        'nose==1.2.1',
+        'httpretty',
+        'nose',
     ]
 )
